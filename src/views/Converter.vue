d7ebb74a8e1a8521ea68793961aaf3374803d831
David <david.moreno92@gmail.com>
2020-04-20 11:38:48 +0200

    Add Task and TaskList components

---
+++ src/views/Converter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Image Converter</h1>
+    <TaskList></TaskList>
+  </div>
+</template>
+
+<script>
+import TaskList from '@/components/TaskList.vue'
+export default {
+  components: {
+    TaskList
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
